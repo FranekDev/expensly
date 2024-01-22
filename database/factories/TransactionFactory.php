64027c4f14fad58229b7554b666fbb148750c9fd
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-01-22 22:35:28 +0100

    Created Transaction model, factory and migration

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'category_id' => fake()->randomDigit(),
+            'amount' => fake()->randomFloat(2),
+            'type' => fake()->randomElement(['expense', 'income']),
+            'user_id' => User::factory(),
+        ];
+    }
+}
