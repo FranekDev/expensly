f377f7c64ab4bd677c406ba2ee5762a23569dc15
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-04-09 22:18:09 +0200

    feat: add models library

---
+++ backend/Expensly/Expensly.Library/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expensly.Library.Models;
+
+public sealed class User
+{
+    [Key]
+    public required int Id { get; set; }
+    
+    [Required]
+    [StringLength(50)]
+    public string UserName { get; set; }
+    
+    [Required]
+    [StringLength(50)]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [StringLength(50)]
+    public string LastName { get; set; }
+    
+    [Required]
+    [StringLength(254)]
+    public string Email { get; set; }
+    
+    [Required]
+    [StringLength(128)]
+    public string Password { get; set; }
+    
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    
+    [Column("updated_at")]
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
