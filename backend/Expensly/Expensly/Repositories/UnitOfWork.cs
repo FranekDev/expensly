91e39dbc5c3e7ddd0084767a13498e9680ba556c
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-04-17 20:56:53 +0200

    feat: add gitignore

---
+++ backend/Expensly/Expensly/Repositories/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using Expensly.Data;
+using Expensly.Library.Models;
+
+namespace Expensly.Repositories;
+
+public class UnitOfWork(ExpenslyContext context) : IDisposable
+{
+    private ExpenslyContext _context = context;
+    private GenericRepository<User> _userRepository;
+
+    public GenericRepository<User> UserRepository
+    {
+        get
+        {
+            if (_userRepository == null)
+            {
+                _userRepository = new GenericRepository<User>(_context);
+            }
+
+            return _userRepository;
+        }
+    }
+
+    public async Task Save()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    private bool _disposed = false;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+
+        _disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
