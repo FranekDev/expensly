04ad514a3c0375f1b89861ec2140d5062ef95d64
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-04-17 21:02:04 +0200

    feat: update user controller to use unitOfWork

---
+++ backend/Expensly/Expensly/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Expensly.Library.Models;
+using Expensly.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expensly.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(UnitOfWork unitOfWork) : ControllerBase
+{
+    private UnitOfWork _unitOfWork = unitOfWork;
+
+    [HttpGet]
+    public Task<ActionResult<IEnumerable<User>>> Get()
+        => _unitOfWork.UserRepository.Get();
+    
+    [HttpPost]
+    [ProducesResponseType(typeof(User), StatusCodes.Status201Created)]
+    public Task<ActionResult<User>> Create([FromBody] User user)
+    {
+        var newUser = _unitOfWork.UserRepository.Create(user);
+        return newUser;
+    }
+}
