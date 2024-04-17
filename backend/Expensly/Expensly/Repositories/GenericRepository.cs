9f2f250311224a85f9ab294f0f4c22f51c0ac5f1
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-04-17 20:59:15 +0200

    feat: create generic repository

---
+++ backend/Expensly/Expensly/Repositories/GenericRepository.cs
@@ -0,0 +1,66 @@
+﻿using Expensly.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expensly.Repositories;
+
+public class GenericRepository<T>(ExpenslyContext context) : ControllerBase, IRepository<T> where T : class
+{
+    protected DbSet<T> dbSet = context.Set<T>();
+    private ExpenslyContext _context => context;
+
+    public async Task<ActionResult<T>> Create(T entity)
+    {
+        await dbSet.AddAsync(entity);
+        return Created();
+    }
+
+    public async Task<ActionResult<T?>> Find(int id)
+    {
+        var item = await dbSet.FindAsync(id);
+
+        if (item is null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(item);
+    }
+
+    public async Task<ActionResult<IEnumerable<T>>> Get()
+    {
+        var data = await dbSet.ToListAsync();
+        return Ok(data);
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var item = await dbSet.FindAsync(id);
+        
+        if (item is null)
+        {
+            return NotFound();
+        }
+
+        Delete(item);
+        
+        return NoContent();
+    }
+
+    public void Delete(T entity)
+    {
+        if (_context.Entry(entity).State == EntityState.Detached)
+        {
+            dbSet.Attach(entity);
+        }
+
+        dbSet.Remove(entity);
+    }
+
+    public async Task<IActionResult> Update(int id, T entity)
+    {
+        dbSet.Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+        return NoContent();
+    }
+}
---
+++ backend/Expensly/Expensly/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Expensly.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    Task<ActionResult<T>> Create(T entity);
+    Task<ActionResult<T?>> Find(int id);
+    Task<ActionResult<IEnumerable<T>>> Get();
+    Task<IActionResult> Delete(int id);
+    Task<IActionResult> Update(int id, T entity);
+}
