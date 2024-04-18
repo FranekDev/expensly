2018487da3c78d530bf8d42cc30ea9a8b9c18811
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-04-18 12:09:34 +0200

    feat: add UserDto

---
+++ backend/Expensly/Expensly.Library/DTOs/UserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Expensly.Library.DTOs;
+
+public class UserDto
+{
+    public int Id { get; init; }
+    
+    public string UserName { get; init; }
+    
+    public string FirstName { get; init; }
+    
+    public string LastName { get; init; }
+    
+    public string Email { get; init; }
+}
