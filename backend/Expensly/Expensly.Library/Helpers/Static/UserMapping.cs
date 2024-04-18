58dcf6f54545f184953ded96f6d757a3c9c496b5
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-04-18 12:06:58 +0200

    feat: add User mapping to UserDto

---
+++ backend/Expensly/Expensly.Library/Helpers/Static/UserMapping.cs
@@ -0,0 +1,21 @@
+﻿using Expensly.Library.DTOs;
+using Expensly.Library.Models;
+
+namespace Expensly.Library.Helpers.Static;
+
+public static class UserMapping
+{
+    public static UserDto MapToDto(this User user)
+    {
+        var dto = new UserDto()
+        {
+            Id = user.Id,
+            UserName = user.UserName,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email
+        };
+        
+        return dto;
+    }
+}
