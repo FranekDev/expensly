d25a571c1bcd5f5123d233aefca103e3453197cc
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-04-20 19:01:23 +0200

    refactor: changed responses in user controller

--- backend/Expensly/Expensly/Controllers/UserController.cs
+++ backend/Expensly/Expensly/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Expensly.Library.Models;
+﻿using Expensly.Library.DTOs;
+using Expensly.Library.Helpers.Static;
+using Expensly.Library.Models;
 using Expensly.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,14 +13,33 @@ public class UserController(UnitOfWork unitOfWork) : ControllerBase
     private UnitOfWork _unitOfWork = unitOfWork;
 
     [HttpGet]
-    public Task<ActionResult<IEnumerable<User>>> Get()
-        => _unitOfWork.UserRepository.Get();
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<UserDto>>> Get()
+    {
+        var data = await _unitOfWork.UserRepository.Get();
+        var mappedData = data.Select(x => x.MapToDto());
+        return Ok(mappedData);
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status302Found)]
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<UserDto>> GetById(int id)
+    {
+        var user = await _unitOfWork.UserRepository.Find(id);
+        if (user is null)
+        {
+            return NotFound();
+        }
+        
+        return user.MapToDto();
+    }
     
     [HttpPost]
     [ProducesResponseType(typeof(User), StatusCodes.Status201Created)]
-    public Task<ActionResult<User>> Create([FromBody] User user)
+    public ActionResult<User> Create([FromBody] User user)
     {
         var newUser = _unitOfWork.UserRepository.Create(user);
-        return newUser;
+        return Created("", newUser);
     }
 }
